5257d7562f870591472ac6ab5f49207da6ede1fb
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-08-15 11:00:25 -0700

    Implemented sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if ((typeof (a) === 'number' && typeof (b) === 'number') && (a >= 0 && b >= 0)) {
+    let accum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      accum += i;
+    }
+    return accum;
+  }
+  return 'ERROR';
+
 
 };
 
