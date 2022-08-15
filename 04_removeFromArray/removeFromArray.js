f8d09138e471cb54f119cc842d17fd7352e8e10d
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-08-15 10:37:56 -0700

    Implemented removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  args.forEach(el => {
+    if (arr.includes(el)) {
+      arr.splice(arr.indexOf(el), 1);
+    }
+  });
+  return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
