c22432576632cfdc33c0e3f4ac95c14dde714e52
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-08-16 13:35:38 -0700

    Implemented palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  let lowerCase = string.toLowerCase();
+  let processedString = '';
+  for (let i = 0; i < lowerCase.length; i++) {
+    let current = lowerCase.charAt(i);
+    if (isLetter(current)) {
+      processedString += current;
+    };
+  }
+
+  let reversed = processedString.split('').reverse().join('');
+
+  for (let i = 0; i < processedString.length; i++) {
+    if (processedString.charAt(i) != reversed.charAt(i)) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+const isLetter = function (char) {
+  return (/[a-z]/).test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
