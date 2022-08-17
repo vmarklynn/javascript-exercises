ddb6403f7e31466c267b19320ca17ca79add84b6
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-08-16 21:46:40 -0700

    Implemented findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge > currentAge ? oldest : current
+  });
 };
+
+const getAge = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
